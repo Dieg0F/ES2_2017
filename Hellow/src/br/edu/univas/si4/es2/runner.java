8c73f7ee9c89ebff51a99068a73cd0095c926693
Diego Fraga <dfraga2012@gmail.com>
2017-08-08 20:52:35 -0300

    Teste Commit 2

---
+++ Hellow/src/br/edu/univas/si4/es2/runner.java
@@ -0,0 +1,11 @@
+package br.edu.univas.si4.es2;
+
+public class runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi!!");		
+	}
+
+}
